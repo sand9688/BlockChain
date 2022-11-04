b70e7b50408126421ace7e895d2239158b03034e
고경훈 <sand9688@naver.com>
2022-11-04 16:19:09 +0900

    맛보기

--- test.sol
+++ test.sol
@@ -1,1 +1,29 @@
 // SPDX-License-Identifier: MIT
+contract HotelRoom {
+
+    enum Statuses { Vacant, Occupied }
+    Statuses currentStatus;
+    address payable public owner;
+
+    event Occupy(address _occupant, uint _value);
+
+    constructor() public {
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently occupied.");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough Ether provided."); 
+        _;
+    } 
+    receive() external payable onlyWhileVacant costs(2 ether) { 
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
